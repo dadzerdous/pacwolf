e91a1cad950f498f61e0ce1beca65ba2c378aa60
dadzerdous <dadzaverous@gmail.com>
2026-04-14 10:48:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "https://dadzerdous.github.io", // Allow your specific GitHub site
-    methods: ["GET", "POST"]
-  }
+  cors: { origin: '*' }
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
